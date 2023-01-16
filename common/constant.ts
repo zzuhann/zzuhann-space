dd8f50fbee0b570e7e8166e07b24c9ec6e7eff9c
zzuhann <zzuhann@zzuhanndeAir.Dlink>
2023-01-16 20:44:48 +0800

    feat: add header and check if logged in to show add post btn

---
+++ common/constant.ts
@@ -0,0 +1,11 @@
+export enum HEADER_NAV {
+  ARTICLE = "article",
+  ABOUT_ME = "about-me",
+  ADD_POST = "add-post",
+}
+
+export const HEADER_NAV_CONTEXT = {
+  [HEADER_NAV.ARTICLE]: "文章列表",
+  [HEADER_NAV.ABOUT_ME]: "關於我",
+  [HEADER_NAV.ADD_POST]: "新增文章",
+};
