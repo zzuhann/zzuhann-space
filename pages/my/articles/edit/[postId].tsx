894fe35322af30d225b10b2123bc0d4595d70edb
zzuhann <zzuhanlin@gmail.com>
2023-02-16 00:34:27 +0800

    feat: edit article and update to firestore

--- pages/my/articles/edit/[postId].tsx
+++ pages/my/articles/edit/[postId].tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
-import { IArticleFirestore } from "../../../../common/articleType";
-import { getDataById } from "../../../../common/firebaseFun";
+import { IArticle } from "../../../../common/articleType";
+import {
+  getDataById,
+  getFirestoreDataById,
+  updateFirestoreById,
+} from "../../../../common/firebaseFun";
 import { Button, Tags, Title } from "../../../../components/addPosts/AddPosts";
 import { ColumnContainer } from "../../../../components/Container";
 import Tiptap from "../../../../components/TipTapEditor";
@@ -10,14 +13,52 @@ import Tiptap from "../../../../components/TipTapEditor";
 const EditArticle = () => {
   const router = useRouter();
   const { postId } = router.query;
-  const [articleDetail, setArticleDetail] = useState<IArticleFirestore>();
+  const [articleDetail, setArticleDetail] = useState<IArticle>();
+  const [tags, setTags] = useState<string[]>([]);
+  const [allOptions, setAllOptions] = useState<string[]>(tags);
+  const [context, setContext] = useState<string>("");
   const titleRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = () => {
+    const newTitle = titleRef.current?.value;
+    const target = "articles";
+    const id = postId;
+    if (!newTitle) return;
+    if (!articleDetail) return;
+    const newArticle: IArticle = {
+      ...articleDetail,
+      content: context,
+      title: newTitle,
+      updateTime: new Date(),
+      tag: allOptions,
+    };
+    updateFirestoreById({ target, id: id as string, data: newArticle });
+    updateFirestoreById({
+      target: "allTags",
+      id: "tags",
+      data: { tags: allOptions },
+    });
+    clear();
+  };
+
+  const clear = () => {
+    if (titleRef.current) {
+      titleRef.current.value = "";
+    }
+    setAllOptions([]);
+    setContext("");
+  };
 
   useEffect(() => {
     const getArticleInfo = () => {
       const collection = "articles";
       const response = getDataById(collection, postId as string);
-      response.then((res) => setArticleDetail(res as IArticleFirestore));
+      response.then((res) => {
+        const articleInfo = res as IArticle;
+        setArticleDetail(articleInfo);
+        setAllOptions(articleInfo.tag);
+        setContext(articleInfo.content);
+      });
     };
     if (postId) {
       getArticleInfo();
@@ -29,18 +70,22 @@ const EditArticle = () => {
       titleRef.current.value = articleDetail.title;
     }
   }, [articleDetail]);
+
+  useEffect(() => {
+    getFirestoreDataById("allTags", "tags", undefined, setTags);
+  }, []);
+
   return (
-    // <h1>home</h1>
     <ColumnContainer>
       <Title titleRef={titleRef} />
-      {/* <Tags
+      <Tags
         tags={tags}
         setTags={setTags}
         allOptions={allOptions}
         setAllOptions={setAllOptions}
       />
-      <Tiptap context={context} setContext={setContext} />
-      <Button onClick={onSubmit}>送出</Button> */}
+      {context && <Tiptap context={context} setContext={setContext} />}
+      <Button onClick={onSubmit}>送出</Button>
     </ColumnContainer>
   );
 };
