7e7f4b4be39115d57ebcab4d3bbe71b33e315d0b
zzuhann <zzuhanlin@gmail.com>
2023-03-05 15:23:06 +0800

    feat: add hyperlink button

--- components/TipTapEditor.tsx
+++ components/TipTapEditor.tsx
@@ -1,20 +1,28 @@
-import { useEditor, EditorContent, ReactNodeViewRenderer } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  ReactNodeViewRenderer,
+  Editor,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import Blockquote from "@tiptap/extension-blockquote";
 import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import PhotoIcon from "@mui/icons-material/Photo";
 import CodeIcon from "@mui/icons-material/Code";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import LinkOffIcon from "@mui/icons-material/LinkOff";
 import css from "highlight.js/lib/languages/css";
 import js from "highlight.js/lib/languages/javascript";
 import ts from "highlight.js/lib/languages/typescript";
 import html from "highlight.js/lib/languages/xml";
 import { lowlight } from "lowlight";
 import CodeBlock from "./CodeBlock";
-import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useCallback } from "react";
 import { uploadStorageImage } from "../common/firebaseFun";
 
 lowlight.registerLanguage("html", html);
@@ -25,14 +33,27 @@ lowlight.registerLanguage("ts", ts);
 const Menubar = ({
   addImage,
   addCodeStyle,
+  setLink,
+  unsetLink,
+  editor,
 }: {
   addImage: (e: ChangeEvent<HTMLInputElement>) => void;
   addCodeStyle: () => void;
+  setLink: () => void;
+  unsetLink: () => void;
+  editor: Editor | null;
 }) => {
   return (
     <div>
       <ImageLabel addImage={addImage} />
       <CodeIcon onClick={addCodeStyle} />
+      <InsertLinkIcon
+        onClick={setLink}
+        className={editor?.isActive("link") ? "is-active" : ""}
+      />
+      <button onClick={unsetLink} disabled={!editor?.isActive("link")}>
+        <LinkOffIcon />
+      </button>
     </div>
   );
 };
@@ -83,6 +104,9 @@ const Tiptap = ({
           return ReactNodeViewRenderer(CodeBlock);
         },
       }).configure({ lowlight }),
+      Link.configure({
+        openOnClick: false,
+      }),
     ],
     content: `${context}`,
     onUpdate: ({ editor }) => {
@@ -109,9 +133,41 @@ const Tiptap = ({
     }
   };
 
+  const setLink = useCallback(() => {
+    if (!editor) return;
+    const previousUrl = editor.getAttributes("link").href;
+    const url = window.prompt("URL", previousUrl);
+
+    // cancelled
+    if (url === null) {
+      return;
+    }
+
+    // empty
+    if (url === "") {
+      editor.chain().focus().extendMarkRange("link").unsetLink().run();
+
+      return;
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange("link").setLink({ href: url }).run();
+  }, [editor]);
+
+  const unsetLink = () => {
+    if (!editor) return;
+    editor.chain().focus().unsetLink().run();
+  };
+
   return (
     <>
-      <Menubar addImage={addImage} addCodeStyle={addCodeStyle} />
+      <Menubar
+        addImage={addImage}
+        addCodeStyle={addCodeStyle}
+        editor={editor}
+        setLink={setLink}
+        unsetLink={unsetLink}
+      />
       <EditorContent editor={editor} />
     </>
   );
