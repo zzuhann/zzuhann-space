04ed2e722af1f27079ed35c13c8eb798b53f750c
zzuhann <zzuhanlin@gmail.com>
2023-04-22 13:53:22 +0800

    ci: set meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="/images/favicon.ico" rel="shortcut icon" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="zzuhann's space to write something I interest in" />
+        <meta name="keywords" content="frontend, university, soft-engineer" />
+        <meta name="author" content="zzuhann" />
+        <meta property="og:title" content="zzuhann's blog" />
+        <meta property="og:description" content="zzuhann's space to write something I interest in" />
+        <meta property="og:image" content="/images/ogimage.jpeg" />
       </Head>
       <body>
         <Main />
